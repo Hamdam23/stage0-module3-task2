8ad53a4f2fbb5d66746a15835c727b3567a9a551
Hamdam <xudayberganovhamdam01@gmail.com>
2022-08-11 10:46:52 +0500

    Naming typo.

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,12 +3,12 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int zero = 0;
-        int twoK = 2000;
-        int oneK = 1000;
-        int fourK = 4000;
-        int threeK = 3000;
-        int fiveK = 5000;
-        int sevenK = 7000;
-        int ninetyK = 90000;
+        int twoThousand = 2000;
+        int oneThousand = 1000;
+        int fourThousand = 4000;
+        int threeThousand = 3000;
+        int fiveThousand = 5000;
+        int sevenThousand = 7000;
+        int ninetyThousand = 90000;
     }
 }
--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int ten = 10;
-        int one = 1;
-        int two = 2;
-        int three = 3;
+        int variableTen = 10;
+        int variableOne = 1;
+        int variableTwo = 2;
+        int variableThree = 3;
         final int VARIABLE_FOUR = 4;
         final int VARIABLE_fIVE = 5;
-        int six = 6;
-        int seven = 7;
-        int eight = 8;
+        int variableSix = 6;
+        int variableSeven = 7;
+        int variableEight = 8;
     }
 }
